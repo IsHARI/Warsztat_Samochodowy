1477190617211d317e11acc3a96273e4014ac8c5
IsHARI <mastoffp@gmail.com>
2018-02-26 23:52:44 +0100

    Implement Client class and Dao

--- sql/db.sql
+++ sql/db.sql
@@ -1,10 +1,14 @@
 CREATE DATABASE crm
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_unicode_ci;
+USE crm;
 
 CREATE TABLE client(
 	id INT AUTO_INCREMENT,
-	
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	email VARCHAR(255),
+	phone VARCHAR(255),
 	PRIMARY KEY(id)
 );
 
---
+++ src/pl/coderslab/model/client/ClientDao.java
@@ -0,0 +1,57 @@
+package pl.coderslab.model.client;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import pl.coderslab.model.Dao;
+import pl.coderslab.model.DataType;
+
+public class ClientDao extends Dao {
+
+	@Override
+	protected String getTableName() {
+		return "client";
+	}
+
+	@Override
+	protected PreparedStatement prepareStatementInsert(DataType object, Connection conn) throws SQLException {
+		Client client = (Client) object;
+
+		PreparedStatement stmt = conn.prepareStatement("INSERT INTO client VALUES(default, ?, ?, ?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
+		stmt.setString(1, client.getFirstName());
+		stmt.setString(2, client.getLastName());
+		stmt.setString(3, client.getEmail());
+		stmt.setString(4, client.getPhone());
+
+		return stmt;
+	}
+
+	@Override
+	protected PreparedStatement prepareStatementUpdate(DataType object, Connection conn) throws SQLException {
+		Client client = (Client) object;
+
+		PreparedStatement stmt = conn.prepareStatement(
+				"UPDATE client SET first_name=?, last_name=?, email=?, phone=? WHERE id=?)",
+				Statement.RETURN_GENERATED_KEYS);
+		stmt.setString(1, client.getFirstName());
+		stmt.setString(2, client.getLastName());
+		stmt.setString(3, client.getEmail());
+		stmt.setString(4, client.getPhone());
+		stmt.setInt(5, client.getId());
+
+		return stmt;
+	}
+
+	@Override
+	protected DataType constructFromRs(ResultSet rs) throws SQLException {
+		Client client = new Client(rs.getString("first_name"), rs.getString("last_name"), rs.getString("email"),
+				rs.getString("phone"));
+		client.setId(rs.getInt("id"));
+		return client;
+	}
+
+}
