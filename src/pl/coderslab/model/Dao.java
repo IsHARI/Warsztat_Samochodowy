bb50933bdf1794f22f9cdda0212007f37b9c6879
IsHARI <mastoffp@gmail.com>
2018-02-26 22:22:19 +0100

    Add update functions to abstract Dao class

--- src/pl/coderslab/model/Dao.java
+++ src/pl/coderslab/model/Dao.java
@@ -4,10 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.coderslab.service.DbUtil;
 
 public abstract class Dao {
+	protected abstract String getTableName();
+	
 	// Insert/Update
 	public void save(DataType object) {
 		try(Connection conn = DbUtil.getConn()) {
@@ -37,7 +41,8 @@ public abstract class Dao {
 	// Delete
 	public void delete(DataType object) {
 		try(Connection conn = DbUtil.getConn()) {
-			PreparedStatement stmt = prepareStatementDelete(object, conn);
+			PreparedStatement stmt = conn.prepareStatement("DELETE FROM " + getTableName() + " WHERE id=?");
+			stmt.setInt(1, object.getId());;
 			stmt.executeUpdate();
 			
 			stmt.close();
@@ -46,5 +51,45 @@ public abstract class Dao {
 		}
 	}
 	
-	protected abstract PreparedStatement prepareStatementDelete(DataType object, Connection conn) throws SQLException;
+	// Select:
+	protected abstract DataType constructFromRs(ResultSet rs) throws SQLException;
+	
+	// All
+	public List<DataType> selectAll() {
+		List<DataType> selected = new ArrayList<>();
+		try(Connection conn = DbUtil.getConn()) {
+			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM " + getTableName());
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				selected.add(constructFromRs(rs));
+			}
+			
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return selected;
+	}
+	
+	// By id
+	public DataType selectById(int id) {
+		DataType selected = null;
+		try(Connection conn = DbUtil.getConn()) {
+			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM " + getTableName() + " WHERE id=?");
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				selected = constructFromRs(rs);
+			}
+			
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return selected;
+	}
 }
